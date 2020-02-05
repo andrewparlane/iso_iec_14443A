0ab5673abc737c4e391eab6d8f4b3dcd1a0dde76
Andrew Parlane <andrew.parlane@gmail.com>
2020-02-05 18:56:44 -0300

    ISO14443A HDL: Moved the pause_n synchroniser to the top level module.
    This is because both the Rx and the FDT modules need the synchronised signal.

--- iso14443a/hdl/iso14443a_top.sv
+++ iso14443a/hdl/iso14443a_top.sv
@@ -44,7 +44,7 @@ module iso14443a_top
     // pause_n is an asynchronous input from the analogue block.
     // It is essentially the digitized envelope of the carrier wave.
     // When idle pause_n is a 1, when a pause is detected it's a 0.
-    // This signal is synchronised in the Rx block
+    // This signal is synchronised before use
     input           pause_n
 );
 
@@ -57,6 +57,23 @@ module iso14443a_top
         .rst_n_out  (rst_n_synchronised)
     );
 
+    // The pause_n signal is asynchronous it can assert / deassert at any point
+    // during the clock cycle. Additionally the clock will not be running during
+    // a pause frame, and so pause_n <may> assert and deassert between rising
+    // clock edges.
+
+    // To handle this we pass it through an active low reset synchroniser.
+    // When pause_n goes low, both FFDs in the synchroniser are reset to 0.
+    // Once pause_n goes high, a 1 is shifted through both FFDs. So two clock ticks
+    // later we detect the rising edge, indicating the end of pause frame.
+
+    logic pause_n_synchronised;
+    active_low_reset_synchroniser pause_n_synchroniser
+    (
+        .clk        (clk),
+        .rst_n_in   (pause_n),
+        .rst_n_out  (pause_n_synchronised)
+    );
 
     logic       soc;
     logic       eoc;
@@ -73,18 +90,18 @@ module iso14443a_top
     )
     rx_inst
     (
-        .clk                (clk),
-        .rst_n              (rst_n_synchronised),
-        .pause_n            (pause_n),
+        .clk                    (clk),
+        .rst_n                  (rst_n_synchronised),
+        .pause_n_synchronised   (pause_n_synchronised),
 
-        .soc                (soc),
-        .eoc                (eoc),
-        .data               (data),
-        .data_bits          (data_bits),
-        .data_valid         (data_valid),
-        .sequence_error     (sequence_error),
-        .parity_error       (parity_error),
-        .last_bit           (last_bit)
+        .soc                    (soc),
+        .eoc                    (eoc),
+        .data                   (data),
+        .data_bits              (data_bits),
+        .data_valid             (data_valid),
+        .sequence_error         (sequence_error),
+        .parity_error           (parity_error),
+        .last_bit               (last_bit)
     );
 
 endmodule
