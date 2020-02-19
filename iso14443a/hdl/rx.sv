ca1df1dade7cf9ddf294a8d23b26edf4763a3f6f
Andrew Parlane <andrewp@carallon.com>
2020-02-19 17:39:15 -0300

    ISO14443A HDL: Removed the EMULATOR_PROJECT parameter.
    I'm giving up on getting this working for now.
    Since Rx was always flakey and Tx doesn't work at all.

--- iso14443a/hdl/rx.sv
+++ iso14443a/hdl/rx.sv
@@ -24,11 +24,6 @@
 `timescale 1ps/1ps
 
 module rx
-#
-(
-    // Some components need slight tweaks to work for the emulator project
-    parameter bit EMULATOR_PROJECT = 0
-)
 (
     // clk is our 13.56MHz input clock. It is recovered from the carrier wave,
     // and as such stops during pause frames. It must not have any glitches.
@@ -65,11 +60,7 @@ module rx
     logic           sd_valid;
     logic           sd_idle;
 
-    sequence_decode
-    #(
-        .EMULATOR_PROJECT   (EMULATOR_PROJECT)
-    )
-    sd_inst
+    sequence_decode sd_inst
     (
         .clk                    (clk),
         .rst_n                  (rst_n),
