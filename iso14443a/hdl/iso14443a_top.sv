cf99e3cdce66673346b87fd76a7c590c0f54cc46
Andrew Parlane <andrew.parlane@gmail.com>
2020-03-16 20:46:00 -0300

    ISO14443A HDL: Updated the top level module to use the part2 and part3 blocks.

--- iso14443a/hdl/iso14443a_top.sv
+++ iso14443a/hdl/iso14443a_top.sv
@@ -26,25 +26,56 @@
 // SDC: create_clock - 13.56MHz +/- 7KHz + extra jitter?
 
 module iso14443a_top
+#(
+    // Are we using single, double or triple UIDs
+    parameter ISO14443A_pkg::UIDSize                                            UID_SIZE,
+
+    // How many UID bits are variable (via the uid input port)?
+    // must be > 0 and <= the total number of bits in the UID
+    // If you want a fixed UID then you can just set this to 1 and hardcode
+    // uid[0] to whatever you want.
+    parameter int                                                               UID_INPUT_BITS,
+
+    // The fixed bits of the UID
+    // the final UID is {UID_FIXED, uid_variable}
+    parameter logic [ISO14443A_pkg::get_uid_bits(UID_SIZE)-UID_INPUT_BITS-1:0]  UID_FIXED,
+
+    // see fdt.sv
+    // TODO: should we add in the delays in frame_encode and tx automatically?
+    //       or expect them to be specified here?
+    // TODO: Maybe add a generate here saying FDT_TIMING_ADJUST MUST BE SET
+    //       default to -1 and if it's not >= 0 then error?
+    parameter int FDT_TIMING_ADJUST
+)
 (
     // clk is our 13.56MHz input clock. It is recovered from the carrier wave,
     // and as such stops during pause frames. It must not have any glitches.
-    input           clk,
+    input                       clk,
 
     // rst_n is an asynchronous active low reset.
     // It passes through a reset synchroniser here.
     // rst_n_synchronised should be used everywhere else.
-    input           rst_n,
+    input                       rst_n,
+
+    // The variable part of the UID
+    // should come from flash or dip switches / wire bonding / hardcoded
+    // I assume this is constant in my code. So I'd recommend only changing it
+    // while this IP core is in reset. That may not be strictly necesarry, but
+    // further investigation would be necesarry to be sure.
+    input [UID_INPUT_BITS-1:0]  uid_variable,
 
     // pause_n is an asynchronous input from the analogue block.
     // It is essentially the digitized envelope of the carrier wave.
     // When idle pause_n is a 1, when a pause is detected it's a 0.
     // This signal is synchronised before use
-    input           pause_n
+    input                       pause_n,
+
+    // tx_out is the manchester encoded data AND'ed with the subcarrier
+    // this should be connected to the load modulator
+    output logic                tx_out
 );
 
     logic rst_n_synchronised;
-
     active_low_reset_synchroniser reset_synchroniser
     (
         .clk        (clk),
@@ -70,29 +101,69 @@ module iso14443a_top
         .rst_n_out  (pause_n_synchronised)
     );
 
-    logic       soc;
-    logic       eoc;
-    logic [7:0] data;
-    logic [2:0] data_bits;
-    logic       data_valid;
-    logic       sequence_error;
-    logic       parity_error;
-    logic       last_bit;
+    rx_interface
+    #(
+        .BY_BYTE    (0)
+    )
+    part2_to_part3_rx_iface
+    (
+        .clk        (clk),
+        .rst_n      (rst_n)
+    );
 
-    rx rx_inst
+    rx_interface
+    #(
+        .BY_BYTE    (1)
+    )
+    part3_to_part4_rx_iface
+    (
+        .clk        (clk),
+        .rst_n      (rst_n)
+    );
+
+    tx_interface
+    #(
+        .BY_BYTE    (0)
+    )
+    part3_to_part2_tx_iface
+    (
+        .clk        (clk),
+        .rst_n      (rst_n)
+    );
+
+    iso14443_2a part2
     (
         .clk                    (clk),
-        .rst_n                  (rst_n_synchronised),
+        .rst_n                  (rst_n),
+
         .pause_n_synchronised   (pause_n_synchronised),
 
-        .soc                    (soc),
-        .eoc                    (eoc),
-        .data                   (data),
-        .data_bits              (data_bits),
-        .data_valid             (data_valid),
-        .sequence_error         (sequence_error),
-        .parity_error           (parity_error),
-        .last_bit               (last_bit)
+        .rx_iface               (part2_to_part3_rx_iface),
+        .tx_iface               (part3_to_part2_tx_iface),
+
+        .tx_out                 (tx_out)
+    );
+
+    iso14443_3a
+    #(
+        .UID_SIZE               (UID_SIZE),
+        .UID_INPUT_BITS         (UID_INPUT_BITS),
+        .UID_FIXED              (UID_FIXED),
+        .FDT_TIMING_ADJUST      (FDT_TIMING_ADJUST+2) // TODO: check this +2 is correct
+    )
+    part3
+    (
+        .clk                    (clk),
+        .rst_n                  (rst_n),
+
+        .uid_variable           (uid_variable),
+
+        .pause_n_synchronised   (pause_n_synchronised),
+
+        .in_rx_iface            (part2_to_part3_rx_iface),
+        .out_rx_iface           (part3_to_part4_rx_iface),
+
+        .out_tx_iface           (part3_to_part2_tx_iface)
     );
 
 endmodule
