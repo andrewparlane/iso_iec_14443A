4b49b85ae1a2dbcd23c96762b617062f72c5643c
Andrew Parlane <andrewp@carallon.com>
2020-05-21 16:34:59 -0300

    VERIFICATION: Ported the iso14443_2a testbenches to the new arch.

---
+++ verification/tb/iso14443_2a/iso14443_2a_tb.sv
@@ -0,0 +1,228 @@
+/***********************************************************************
+        File: iso14443_2a_tb.sv
+ Description: A loopback based testbench for the iso14443_2a module
+      Author: Andrew Parlane
+**********************************************************************/
+
+/*
+ * This file is part of https://github.com/andrewparlane/fiuba_thesis/blob/master/LICENSE
+ * Copyright (c) 2020 Andrew Parlane.
+ *
+ * This is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this code. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+`timescale 1ps/1ps
+
+module iso14443_2a_tb;
+
+    // --------------------------------------------------------------
+    // Ports to DUT
+    // all named the same as in the DUT, so I can use .*
+    // --------------------------------------------------------------
+
+    logic       clk;
+    logic       rst_n;
+
+    logic       pause_n_synchronised;
+
+    rx_interface #(.BY_BYTE(0)) rx_iface(.*);
+    tx_interface #(.BY_BYTE(0)) tx_iface(.*);
+
+    logic       tx_out;
+
+    // --------------------------------------------------------------
+    // DUT
+    // --------------------------------------------------------------
+
+    iso14443_2a dut (.*);
+
+    // --------------------------------------------------------------
+    // The source for the clock and pause_n signal
+    // includes the pcd_pause_n driver
+    // --------------------------------------------------------------
+    analogue_sim analogue_sim_inst
+    (
+        .picc_clk               (clk),
+        .pcd_pause_n            (),
+        .pause_n_async          (),
+        .pause_n_synchronised   (pause_n_synchronised)
+    );
+
+    // our Rx send queue
+    typedef pcd_pause_n_transaction_pkg::PCDPauseNTransaction RxSendTransType;
+    RxSendTransType rx_send_queue [$];
+
+    // --------------------------------------------------------------
+    // The monitor for the rx_iface (output)
+    // --------------------------------------------------------------
+
+    rx_bit_iface_monitor_pkg::RxBitIfaceMonitor rx_monitor;
+
+    // and the Rx recv_queue
+    typedef rx_bit_transaction_pkg::RxMonitorBitTransaction RxRecvTransType;
+    RxRecvTransType rx_recv_queue [$];
+
+    // --------------------------------------------------------------
+    // The source driver / queue for the tx_iface
+    // --------------------------------------------------------------
+
+    // driver
+    tx_bit_iface_source_driver_pkg::TxBitIfaceSourceDriver tx_driver;
+
+    // the send queue
+    typedef tx_bit_transaction_pkg::TxBitTransaction TxTransType;
+    TxTransType tx_send_queue[$];
+
+    // --------------------------------------------------------------
+    // The monitor for the load modulator
+    // --------------------------------------------------------------
+
+    // monitor
+    load_modulator_monitor_pkg::LoadModulatorMonitor lm_monitor;
+
+    // recv queue
+    TxTransType tx_recv_queue   [$];
+
+    // and what we expect to receive
+    TxTransType tx_expected     [$];
+
+    // interface
+    load_modulator_iface lm_iface (.*);
+    assign lm_iface.lm = tx_out;
+
+    // --------------------------------------------------------------
+    // The loopback
+    // --------------------------------------------------------------
+    always @(posedge clk, negedge rst_n) begin
+        if (!rst_n) begin
+            tx_send_queue = '{};
+        end
+        else begin
+            if (rx_recv_queue.size() != 0) begin
+                automatic RxRecvTransType rx_trans = rx_recv_queue.pop_front();
+
+                // drop error frames
+                if (!rx_trans.error) begin
+                    automatic TxTransType tx_trans = new(rx_trans.data);
+                    tx_send_queue.push_back(tx_trans);
+                end
+            end
+        end
+    end
+
+    // --------------------------------------------------------------
+    // The verification stuff
+    // --------------------------------------------------------------
+    int check_count;
+
+    always @(posedge clk, negedge rst_n) begin: verificationBlock
+        if (!rst_n) begin
+            check_count = 0;
+        end
+        else begin: posedgeClk
+            // wait for there to be an expected transaction and an actual transaction
+            if ((tx_recv_queue.size() != 0) &&
+                (tx_expected.size() != 0)) begin: readyToCompare
+                // pop and compare
+                automatic TxTransType received = tx_recv_queue.pop_front();
+                automatic TxTransType expected = tx_expected.pop_front();
+
+                dataAsExpected:
+                assert (received.compare(expected)) else $error("Received %s, not as expected %p",
+                        received.to_string, expected.to_string);
+
+                check_count++;
+                if ((check_count % 100) == 0) begin
+                    $display("Received %d packets", check_count);
+                end
+            end
+        end
+    end
+
+    // --------------------------------------------------------------
+    // Test stimulus
+    // --------------------------------------------------------------
+
+    initial begin: testStimulus
+        automatic int timeout;
+
+        analogue_sim_inst.init();
+        rx_monitor  = new(rx_iface);
+        tx_driver   = new(tx_iface, 256, 256);  // 256 bits of idle after each tx (2 bit times)
+        lm_monitor  = new(lm_iface);
+
+        rx_send_queue   = '{};
+        rx_recv_queue   = '{};
+        tx_send_queue   = '{};
+        tx_recv_queue   = '{};
+        tx_expected     = '{};
+
+        analogue_sim_inst.start(rx_send_queue);
+        rx_monitor.start(rx_recv_queue);
+        tx_driver.start(tx_send_queue);
+        lm_monitor.start(tx_recv_queue);
+
+        // reset for 5 ticks
+        rst_n <= 1'b0;
+        repeat (5) @(posedge clk) begin end
+        rst_n <= 1'b1;
+        repeat (5) @(posedge clk) begin end
+
+        timeout = 0;
+        repeat(1000) begin: loops
+            automatic RxRecvTransType   bit_trans;
+            automatic RxSendTransType   pcd_trans;
+            automatic TxTransType       expected;
+
+            bit_trans = RxRecvTransType::new_random_transaction($urandom_range(1, 80), 1'b0);
+            pcd_trans = new(bit_trans.convert_to_pcd_sequence_queue);
+            expected = new(bit_trans.data);
+
+            //$display("bit_trans: %s", bit_trans.to_string);
+            //$display("pcd_trans: %s", pcd_trans.to_string);
+            //$display("expected: %s", expected.to_string);
+
+            timeout += analogue_sim_inst.driver.calculate_send_time(pcd_trans);
+
+            rx_send_queue.push_back(pcd_trans);
+            tx_expected.push_back(expected);
+        end
+
+        // wait for the PCD driver to be idle
+        analogue_sim_inst.driver.wait_for_idle(timeout + 100);
+        $display("rx_driver now idle (or timedout)");
+        // wait for the rx_monitor to be idle
+        rx_monitor.wait_for_idle(16, 32);
+        $display("rx_monitor now idle (or timedout)");
+        // wait for the tx_driver to be idle infinite timeout, because each send can time out
+        tx_driver.wait_for_idle();
+        $display("tx_driver now idle (or timedout)");
+        // wait for the tx_monitor to be idle
+        lm_monitor.wait_for_idle(64, 128);
+        $display("lm_monitor now idle (or timedout)");
+
+        // finally check that all the queues are empty
+        queuesEmpty:
+        assert ((rx_send_queue.size()   == 0) &&
+                (rx_recv_queue.size()   == 0) &&
+                (tx_send_queue.size()   == 0) &&
+                (tx_recv_queue.size()   == 0) &&
+                (tx_expected.size()     == 0))
+            else $error("Not all queues are empty at end of test");
+
+        repeat (5) @(posedge clk) begin end
+        $stop;
+    end
+
+
+endmodule
--- verification/tb/iso14443_2a/tx_tb.sv
+++ verification/tb/iso14443_2a/tx_tb.sv
@@ -45,66 +45,76 @@ module tx_tb;
     tx dut (.*);
 
     // --------------------------------------------------------------
-    // The source for the in_iface
+    // The source driver / queue for the in_iface
     // --------------------------------------------------------------
 
-    tx_interface_source tx_source
-    (
-        .clk    (clk),
-        .iface  (in_iface)
-    );
+    // driver
+    tx_bit_iface_source_driver_pkg::TxBitIfaceSourceDriver driver;
+
+    // the send queue
+    typedef tx_bit_transaction_pkg::TxBitTransaction TransType;
+    TransType send_queue[$];
 
     // --------------------------------------------------------------
-    // The sink for the load modulator (tx_out)
+    // The monitor for the load modulator (tx_out)
     // --------------------------------------------------------------
 
-    load_modulator_sink lm_sink (.*);
+    // monitor
+    load_modulator_monitor_pkg::LoadModulatorMonitor monitor;
+
+    // recv queue
+    TransType recv_queue[$];
+
+    // interface
+    load_modulator_iface lm_iface (.*);
+    assign lm_iface.lm = tx_out;
 
     // --------------------------------------------------------------
     // Clock generator
     // --------------------------------------------------------------
 
-    // Calculate our clock period in ps
-    localparam CLOCK_FREQ_HZ = 13560000; // 13.56MHz
-    localparam CLOCK_PERIOD_PS = 1000000000000.0 / CLOCK_FREQ_HZ;
-    initial begin
-        clk = 1'b0;
-        forever begin
-            #(int'(CLOCK_PERIOD_PS/2))
-            clk = ~clk;
-        end
-    end
+    clock_source clock_source_inst (.*);
 
     // --------------------------------------------------------------
     // Functions / Tasks
     // --------------------------------------------------------------
 
-    task send_data (logic sq[$]);
-        automatic logic rq [$];
-        //$display("sending %p", sq);
-
+    task send_data_verify_result(TransType trans);
         // send it
-        tx_source.send_frame(sq);
-        lm_sink.wait_for_idle(512);
+        send_queue.push_back(trans);
 
-        // get the received data
-        rq = lm_sink.get_and_clear_received_queue();
+        // wait for the monitor to be idle
+        // wait for 16 ticks of idle in a row - this lets the driver get started
+        // each bit takes 128 ticks to send, and we have 1 bit of idle and 1 bit of SOC
+        // on top of the data bits, then +2 extra just to be sure.
+        monitor.wait_for_idle(16, ((trans.data.size() + 4) * 128));
 
-        // check we got what we sent + the SOC bit
-        sq.push_front(1'b1);
-        receivedAsExpected: assert(rq == sq)
-            else $error("received not as expected, got %p expected %p", rq, sq);
+        // verify
+        receivedOneTransaction:
+        assert (recv_queue.size() == 1) else $error("recv_queue.size() is %d, expecting 1", recv_queue.size());
+
+        if (recv_queue.size() != 0) begin: recvQueueNotEmpty
+            automatic TransType recv = recv_queue.pop_front;
+            receivedExpected:
+            assert (recv.compare(trans)) else $error("Received %s, not as expected %p", recv.to_string, trans.to_string);
+        end
     endtask
 
     // --------------------------------------------------------------
     // Test stimulus
     // --------------------------------------------------------------
 
-    initial begin
+    initial begin: testStimulus
         automatic logic sendQueue[$] = '{};
 
-        tx_source.initialise;
-        lm_sink.initialise;
+        driver  = new(in_iface, 8, 256);
+        monitor = new(lm_iface);
+
+        send_queue = '{};
+        recv_queue = '{};
+
+        driver.start(send_queue);
+        monitor.start(recv_queue);
 
         // reset for 5 ticks
         rst_n <= 1'b0;
@@ -113,24 +123,22 @@ module tx_tb;
         repeat (5) @(posedge clk) begin end
 
         // Stuff to test
-        //  0) SOC - done by add the SOC bit to expected queue
-        //  1) nothing sends if data_valid is low
-        in_iface.data_valid <= 1'b0;
+        //  0) SOC - done in the monitor by checking for a '1' and removing not adding it to the transaction
+        //  1) nothing sends if data_valid is low (done by the driver)
         repeat (512) @(posedge clk) begin end
-        // we'll error later if any data was received by the lm_sink
+        nothingSentWhenDataValidLow:
+        assert (monitor.idle && (recv_queue.size() == 0))
+            else $error("With data_valid low, the monitor is either not idle (%b) or has received some frames %d",
+                        monitor.idle, recv_queue.size());
 
-        //  2) correct number of bits are sent
-        //      - implicity checked by adding all bits to the expected queue
-        //        and by adding 2 bit times of idle to the end of the expected queue
+        //  2) correct data is set
+        for (int j = 1; j <= 100; j++) begin
+            // create a transaction of a random number of random bits
+            automatic TransType trans = TransType::new_random_transaction($urandom_range(1, 100));
 
-        for (int i = 0; i < 100; i++) begin
-            // add a random number of random bits to the sendQueue
-            automatic int bits_to_send = $urandom_range(1, 100);
-            $display("%d/100 - sending %d bits", i, bits_to_send);
-            for (int i = 0; i < bits_to_send; i++) begin
-                sendQueue.push_back(1'($urandom));
-            end
-            send_data(sendQueue);
+            $display("%d/100 - sending %d bits", j, trans.data.size);
+
+            send_data_verify_result(trans);
 
             repeat (5) @(posedge clk) begin end
         end
