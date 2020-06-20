65f4c2fa7ebe3256e11144bb1dde9be8380660b3
Andrew Parlane <andrewp@carallon.com>
2020-06-20 12:09:11 -0300

    ISO14443A HDL: Updated the top level module.
    synchronise the async power input.
    renamed some inputs to _async to make it clearer which are the async and
    which are the synchronous signals.
    Export the part4 <-> app rx and tx interfaces.
    Added extensive comments on the FDT timer.
    Accounted for delays in the part2 block when passing FDT_TIMING_ADJUST
    to the part3 block.
    Error if FDT_TIMING_ADJUST is not assigned.

--- iso14443a/hdl/iso14443a_top.sv
+++ iso14443a/hdl/iso14443a_top.sv
@@ -40,22 +40,53 @@ module iso14443a_top
     // the final UID is {UID_FIXED, uid_variable}
     parameter logic [ISO14443A_pkg::get_uid_bits(UID_SIZE)-UID_INPUT_BITS-1:0]  UID_FIXED,
 
-    // see fdt.sv
-    // TODO: should we add in the delays in frame_encode and tx automatically?
-    //       or expect them to be specified here?
-    // TODO: Maybe add a generate here saying FDT_TIMING_ADJUST MUST BE SET
-    //       default to -1 and if it's not >= 0 then error?
-    parameter int FDT_TIMING_ADJUST
+    // see fdt.sv for more details.
+    // We have already accounted for the delay in the internal Tx path. We pass the
+    // FDT_TIMING_ADJUST parametr with a +3 below and a +1 in the framing module.
+    // All remaining delays can not be calculated here, as they depend on the AFE.
+    //
+    // This parameter should be set as follows:
+    //      PCD_PAUSE_N_TO_SYNCHRONISED_PS - This should be the time in ps between the start of
+    //                                       the rising edge of the analogue RF signal (see small
+    //                                       circles in Figure 3 of ISO/IEC 14443-2:2016, I think
+    //                                       the one labled as 5, but the docs aren't clear),
+    //                                       and the rising edge of the pause_n_synchronised signal.
+    //                                       We can't account for the delay in the synchroniser
+    //                                       automatically, because it depends on when the end of pause
+    //                                       is detected and when the clock starts up again.
+    //                                       if the end of pause is detected before the clock starts
+    //                                       then the synchronisation time is 1 or 1.5 ticks.
+    //                                       otherwise it's between 1 and 2 ticks after the end
+    //                                       of pause is detected.
+    //
+    //      TX_OUT_TO_MODULATION_EDGE_PS   - This should be the time in ps between the rising edge of
+    //                                       the tx_out signal and the load modulator activating.
+    //                                       Which includes any output buffering, and propagation delays
+    //                                       in the analogue front end.
+    //
+    // Both of these should be set to the MINIMUM possible values, since the spec states that we can
+    // be a up to 0.4us late but must not be early.
+    //
+    // Finally FDT_TIMING_ADJUST should be set to the sum of these divided by the MAXIMUM possible
+    // clock period and rounded DOWN. (MAX clock period and rounded DOWN, due to the same reason as
+    // above).
+    //
+    // FDT_TIMING_ADJUST = $rtoi((PCD_PAUSE_N_TO_SYNCHRONISED_PS + TX_OUT_TO_MODULATION_EDGE_PS)
+    //                              / MAX_CLOCK_PERIOD_PS);
+    // where MAX_CLOCK_PERIOD_PS is 1/(13.56MHz - 7KHz) = 73,784.4 ps
+    //
+    // Making it signed, and the default -1, so I can generate an error if it's not correctly set
+    parameter int signed FDT_TIMING_ADJUST = -1
 )
 (
     // clk is our 13.56MHz input clock. It is recovered from the carrier wave,
     // and as such stops during pause frames. It must not have any glitches.
     input                       clk,
 
-    // rst_n is an asynchronous active low reset.
+    // rst_n_async is an asynchronous active low reset.
     // It passes through a reset synchroniser here.
-    // rst_n_synchronised should be used everywhere else.
-    input                       rst_n,
+    // rst_n should be used everywhere else.
+    input                       rst_n_async,
 
     // The variable part of the UID
     // should come from flash or dip switches / wire bonding / hardcoded
@@ -64,72 +95,97 @@ module iso14443a_top
     // further investigation would be necesarry to be sure.
     input [UID_INPUT_BITS-1:0]  uid_variable,
 
-    // pause_n is an asynchronous input from the analogue block.
+    // Every reply from the PICC that includes a CID field also includes a power indicator
+    // field to tell the PCD if it's receiving enough power or not. The PCD can then adjust
+    // it's power output as needed.
+    //
+    // The analogue block should pass the correct value in. It can change over time.
+    // and is synchronised here.
+    //
+    // ISO/IEC 14443-4:2016 section 7.4 states:
+    //      2'b00: PICC does not support the power level indiction
+    //      2'b01: Insufficient power for full functionality
+    //      2'b10: Sufficient power for full functionality
+    //      2'b11: More than sufficient power for full functionality
+    input [1:0]                 power_async,
+
+    // pause_n_async is an asynchronous input from the analogue block.
     // It is essentially the digitized envelope of the carrier wave.
-    // When idle pause_n is a 1, when a pause is detected it's a 0.
+    // When idle pause_n_async is a 1, when a pause is detected it's a 0.
     // This signal is synchronised before use
-    input                       pause_n,
+    input                       pause_n_async,
 
     // tx_out is the manchester encoded data AND'ed with the subcarrier
     // this should be connected to the load modulator
-    output logic                tx_out
+    output logic                tx_out,
+
+    // The interface with the application.
+    // The INF field of received STD I-Blocks is forwarded to the app on the app_rx_iface.
+    // The app may respond with the INF field using the app_tx_iface.
+    // When app_resend_last asserts, the app should send the same reply as it did to the last message.
+    rx_interface.out_byte       app_rx_iface,
+    tx_interface.in_byte        app_tx_iface,
+    output logic                app_resend_last
 );
 
-    logic rst_n_synchronised;
+    // check the FDT_TIMING_ADJUST signal has been set
+    generate
+        if (FDT_TIMING_ADJUST < 0) begin
+            synth_time_error fdt_timing_adjust_must_be_set(.*);
+        end
+    endgenerate
+
+    // ========================================================================
+    // Synchronisation of async signals
+    // ========================================================================
+
+    // syncronise our asynchronous reset
+    logic rst_n;
     active_low_reset_synchroniser reset_synchroniser
     (
         .clk        (clk),
-        .rst_n_in   (rst_n),
-        .rst_n_out  (rst_n_synchronised)
+        .rst_n_in   (rst_n_async),
+        .rst_n_out  (rst_n)
     );
 
-    // The pause_n signal is asynchronous it can assert / deassert at any point
+    // The pause_n_async signal is asynchronous it can assert / deassert at any point
     // during the clock cycle. Additionally the clock will not be running during
-    // a pause frame, and so pause_n <may> assert and deassert between rising
+    // a pause frame, and so pause_n_async <may> both assert and deassert between rising
     // clock edges.
 
     // To handle this we pass it through an active low reset synchroniser.
-    // When pause_n goes low, both FFDs in the synchroniser are reset to 0.
-    // Once pause_n goes high, a 1 is shifted through both FFDs. So two clock ticks
-    // later we detect the rising edge, indicating the end of pause frame.
+    // When pause_n_async goes low, both FFDs in the synchroniser are reset to 0.
+    // Once pause_n_async goes high, a 1 is shifted through both FFDs. So two clock ticks
+    // later we detect the rising edge, indicating the end of the pause frame.
 
     logic pause_n_synchronised;
     active_low_reset_synchroniser pause_n_synchroniser
     (
         .clk        (clk),
-        .rst_n_in   (pause_n),
+        .rst_n_in   (pause_n_async),
         .rst_n_out  (pause_n_synchronised)
     );
 
-    rx_interface
+    logic [1:0] power;
+    synchroniser
     #(
-        .BY_BYTE    (0)
+        .WIDTH      (2),
+        .RESET_VAL  (2'b00)
     )
-    part2_to_part3_rx_iface
+    power_synch_inst
     (
         .clk        (clk),
-        .rst_n      (rst_n)
+        .rst_n      (rst_n),
+        .d          (power_async),
+        .q          (power)
     );
 
-    rx_interface
-    #(
-        .BY_BYTE    (1)
-    )
-    part3_to_part4_rx_iface
-    (
-        .clk        (clk),
-        .rst_n      (rst_n)
-    );
+    // ========================================================================
+    // ISO/IEC 14443-2A
+    // ========================================================================
 
-    tx_interface
-    #(
-        .BY_BYTE    (0)
-    )
-    part3_to_part2_tx_iface
-    (
-        .clk        (clk),
-        .rst_n      (rst_n)
-    );
+    rx_interface #(.BY_BYTE(0)) part2_to_part3_rx_iface (.*);
+    tx_interface #(.BY_BYTE(0)) part3_to_part2_tx_iface (.*);
 
     iso14443_2a part2
     (
@@ -144,12 +200,27 @@ module iso14443a_top
         .tx_out                 (tx_out)
     );
 
+    // ========================================================================
+    // ISO/IEC 14443-3A
+    // ========================================================================
+
+    rx_interface #(.BY_BYTE(1)) part3_to_part4_rx_iface (.*);
+    tx_interface #(.BY_BYTE(1)) part4_to_part3_tx_iface (.*);
+
+    logic part4_rx_crc_ok;
+    logic part4_deselect;
+    logic part4_rats;
+    logic part4_tag_active;
+    logic part4_tx_append_crc;
+
     iso14443_3a
     #(
         .UID_SIZE               (UID_SIZE),
         .UID_INPUT_BITS         (UID_INPUT_BITS),
         .UID_FIXED              (UID_FIXED),
-        .FDT_TIMING_ADJUST      (FDT_TIMING_ADJUST+2) // TODO: check this +2 is correct
+        // We use +3 here because of delays in the Tx path in the iso14443_2a module
+        // see the comments at the start of fdt.sv
+        .FDT_TIMING_ADJUST      (FDT_TIMING_ADJUST+3)
     )
     part3
     (
@@ -160,10 +231,43 @@ module iso14443a_top
 
         .pause_n_synchronised   (pause_n_synchronised),
 
-        .in_rx_iface            (part2_to_part3_rx_iface),
-        .out_rx_iface           (part3_to_part4_rx_iface),
+        .rx_iface_from_14443_2a (part2_to_part3_rx_iface),
+        .tx_iface_to_14443_2a   (part3_to_part2_tx_iface),
 
-        .out_tx_iface           (part3_to_part2_tx_iface)
+        .rx_iface_to_14443_4a   (part3_to_part4_rx_iface),
+        .rx_crc_ok              (part4_rx_crc_ok),
+        .tx_iface_from_14443_4a (part4_to_part3_tx_iface),
+        .tx_append_crc_14443_4a (part4_tx_append_crc),
+
+        .iso14443_4a_deselect   (part4_deselect),
+        .iso14443_4a_rats       (part4_rats),
+        .iso14443_4a_tag_active (part4_tag_active)
+    );
+
+    // ========================================================================
+    // ISO/IEC 14443-4A
+    // ========================================================================
+
+    iso14443_4a part4
+    (
+        .clk                    (clk),
+        .rst_n                  (rst_n),
+
+        .power                  (power),
+
+        .rx_iface               (part3_to_part4_rx_iface),
+        .rx_crc_ok              (part4_rx_crc_ok),
+        .tx_iface               (part4_to_part3_tx_iface),
+        .tx_append_crc          (part4_tx_append_crc),
+
+        .tag_active             (part4_tag_active),
+
+        .rx_rats                (part4_rats),
+        .rx_deselect            (part4_deselect),
+
+        .app_rx_iface           (app_rx_iface),
+        .app_tx_iface           (app_tx_iface),
+        .app_resend_last        (app_resend_last)
     );
 
 endmodule
